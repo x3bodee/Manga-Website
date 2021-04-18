b9b2c7e7d3e0e944e37fafbb167aecaea221a90a
hndfhd <hndfhd1413@gmail.com>
2021-04-18 14:39:29 +0300

    add login -logout -auth

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 const express = require("express");
 
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT;
 
 const expresslayouts = require("express-ejs-layouts");
-//var flash = require('connect-flash');
+var flash = require('connect-flash');
 
 // Initialize express
 const app = express();
@@ -18,85 +18,77 @@ app.use(express.static("public"));
 // Look into the views folder for layout.ejs file
 app.use(expresslayouts);
 
-// Express Session and Passport
-//let session = require('express-session');
-//let passport = require('./helper/ppConfig');
+//Express Session and Passport
+let session = require('express-session');
+let passport = require('./helper/ppConfig');
 
-// app.use(session({
-//     secret: process.env.SECRET,
-//     saveUninitialized: true,
-//     resave: false,
-//     cookie: {maxAge: 36000000}
-// }))
+app.use(session({
+  secret: process.env.SECRET,
+  saveUninitialized: true,
+  resave: false,
+  cookie: {
+    maxAge: 36000000
+  }
+}))
 
 // Initialize Passport and Passport Session
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.use(flash());
+app.use(flash());
 
 // // Sharing information to other pages
-// app.use(function(req, res, next){
-//     res.locals.currentUser = req.user;
-//     res.locals.alerts = req.flash();
-//     next();
-// })
+ app.use(function(req, res, next){
+     res.locals.currentUser = req.user;
+     res.locals.alerts = req.flash();
+     next();
+ })
 
-app.set('view engine' , 'ejs');
+app.set('view engine', 'ejs');
 
 
 
 // routes
-//app.use(require('./routes/user'))
-//app.use(require('./routes/twitt'))
+app.use(require('./routes/Auth'))
+//app.use(require('./routes/'))
 
 //console.log(process.env.mongoDBURL);
 
-// mongoose.connect(
-//     process.env.mongoDBURL,
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex: true
-//     },
-//     () => {
-//       console.log("Mongodb connected seccessfully!!!");
-//     }
-//   );
+mongoose.connect(
+    process.env.mongoDBURL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    },
+    () => {
+      console.log("Mongodb connected seccessfully!!!");
+    }
+  );
 
-app.listen(PORT , () => console.log(`server manga work now ${PORT} `))
+app.listen(PORT, () => console.log(`server manga work now ${PORT} `))
 
-app.get('/' , (req ,res ) => {
+app.get('/', (req, res) => {
 
   res.render('home/home')
- 
+
 })
 
 
-app.get('/manga' , (req ,res ) => {
+app.get('/manga', (req, res) => {
 
   res.render('manga/show')
- 
+
 })
 
-app.get('/manga/add' , (req ,res ) => {
+app.get('/manga/add', (req, res) => {
 
   res.render('manga/add')
- 
+
 })
 
-app.get('/chapter' , (req ,res ) => {
+app.get('/chapter', (req, res) => {
 
   res.render('chapter/show')
- 
-})
-app.get('/login' , (req ,res ) => {
 
-  res.render('auth/login')
- 
-})
-app.get('/signup' , (req ,res ) => {
-
-  res.render('auth/signup')
- 
 })
